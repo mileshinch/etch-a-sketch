b67b526e0a166bf4d9e3d2b8eba912fc0a3a83ad
mileshinch <miles.hinchliffe@levellingup.gov.uk>
2024-12-19 09:49:14 +0000

    Create function that is called from button click
    Put my code to build a grid and enable drawing into functions that are
    called when the user clicks the button and enters a number.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,11 +1,7 @@
-// get container as javascript obj
-const container = document.querySelector(".container")
-
-// fill the container with 16 rows
-
-let n = 40
-
-for (let i = 1; i <= n; i++) {
+// CREATE GRID FUNCTION
+function createGrid(numberOfSides) {
+  // fill the container with 16 rows
+  for (let i = 1; i <= numberOfSides; i++) {
     // add a row div as js obj
     const row = document.createElement("div")
     // give it a class
@@ -14,7 +10,7 @@ for (let i = 1; i <= n; i++) {
     row.style.outline = "1px dashed"
 
     // for each row fill it with 16 squares
-    for (let j = 1; j <= n; j++) {
+    for (let j = 1; j <= numberOfSides; j++) {
         // add a square as js obj
         const square = document.createElement("div")
         // give it a class of square
@@ -27,9 +23,12 @@ for (let i = 1; i <= n; i++) {
 
     // add it to the dom
     container.appendChild(row)
+  }
 }
 
-// create a square object
+// ENABLE DRAWING FUNCTION
+function enableDrawing() {
+  // create a square node list
 const squares = document.querySelectorAll(".square")
 // set up an event listener for all the squares
 squares.forEach((square) => {
@@ -39,18 +38,41 @@ squares.forEach((square) => {
       square.style.backgroundColor = "#00008B"
     });
   });
+}
 
 // create a button in the DOM
-
 const btn = document.createElement("button")
-btn.innerText = "Create grid"
+btn.innerText = "Create new grid"
 // append button to body
 const body = document.querySelector("body")
 body.appendChild(btn)
 
+// get container as javascript obj
+const container = document.querySelector(".container")
+
+createGrid(16)
+
 btn.addEventListener("click", () => {
-  let numOfSquares = prompt("How many squares per side do you want? Enter a number between 1 and 100","100")
+  // prompt asking for number of squares
+  let numberOfSides = prompt("How many squares per side do you want? Enter a number between 1 and 100","100")
+  // removes the existing grid
+  const grid = document.querySelectorAll(".row")
+  grid.forEach((div) => {
+    div.remove()
+  })
+  // adds a new grid
+  createGrid(numberOfSides)
+  // enable drawing
+  enableDrawing()
 })
+
+
+
+
+
+
+
+
 
 
 
